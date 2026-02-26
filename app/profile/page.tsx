bb7dc87d5e9fe40c82314417f4e3aaf507c12126
Your Name <your_email@example.com>
2026-02-26 18:08:51 +0100

    Checking if login page works

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,9 +2,11 @@
 
 export default function ProfilePage() {
   return (
-    <div>
-      <h1>Profile</h1>
-      <p>You are logged in.</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold mb-6">Profile</h1>
+      <p className="text-8xl font-extrabold text-green-600">
+        You are logged in.
+      </p>
     </div>
   );
 }
